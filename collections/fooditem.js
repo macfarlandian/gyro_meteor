b44ab9be1ebd6124b458da16ae68311e0b9bcc70
Paul Young <paulandresyoung@gmail.com>
2014-07-26 14:57:38 -0700

    removed dummy data insert on startup

--- collections/fooditem.js
+++ collections/fooditem.js
@@ -1,12 +1,1 @@
 FoodItems = new Meteor.Collection("FoodItems");
-
-if (Meteor.isServer){
-    Meteor.startup(
-    function (){
-        FoodItems.insert( {
-            name: "FOOD!!!",
-            startdate: new Date(),
-        });
-    }
-    );
-}
